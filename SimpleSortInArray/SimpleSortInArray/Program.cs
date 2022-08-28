505c864ec5b6a7390be7a883322d80eaaccf20cd
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-28 09:49:36 +0430

    Initial Repo

---
+++ SimpleSortInArray/SimpleSortInArray/Program.cs
@@ -0,0 +1,7 @@
+﻿using SimpleSortInArray;
+
+int[] array = { 5, 4, 3,3,3,-40,-10000,7878787, 2, 1 };
+
+SimpleSort sort = new SimpleSort();
+
+sort.Sort(array);
---
+++ SimpleSortInArray/SimpleSortInArray/SimpleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSortInArray
+{
+    public class SimpleSort
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if(array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
